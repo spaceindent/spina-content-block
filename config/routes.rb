9bc6df6e3bf481f3375612577e77a41a50e1f071
Michael Wawra <michael@wawra.co.uk>
2017-07-19 21:41:51 +0100

    (feature) Adding routes
    Testing this always seems a bit pointless to me - it's testing Rails...

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,5 @@
 Spina::Engine.routes.draw do
-  namespace :blog do
-    root to: 'posts#index'
-
-    resources :posts, only: [:index, :show]
-
-    get 'feed.atom', to: 'posts#index', as: :rss_feed, defaults: { format: :atom }
-    get 'categories/:id', to: 'categories#show', as: :category
-    get 'archive/:year(/:month)', to: 'posts#archive', as: :archive_posts
-  end
-
   namespace :admin do
-    namespace :blog do
-      resources :categories
-      resources :posts do
-        collection do
-          get :live
-          get :draft
-          get :future
-        end
-      end
-    end
+    resources :blocks
   end
 end
